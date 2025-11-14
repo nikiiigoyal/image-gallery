0d9f2c12676acb59e9ce49aa0a9261c18a8eb663
Nikita Goyal <nikiiigoyal@gmail.com>
2025-11-14 13:59:02 +0530

    Add images

--- src/index.jsx
+++ src/index.jsx
@@ -7,16 +7,16 @@ const images = [
   // Front
   { position: [0, 0, 1.5], rotation: [0, 0, 0], url: '/bayc1.avif' },
   // Back
-  { position: [-0.8, 0, -0.6], rotation: [0, 0, 0], url: '/bayc2.png' },
-  { position: [0.8, 0, -0.6], rotation: [0, 0, 0], url: '/bayc3.jpeg' },
+  { position: [-0.8, 0, -0.6], rotation: [0, 0, 0], url: '/bayc2.avif' },
+  { position: [0.8, 0, -0.6], rotation: [0, 0, 0], url: '/bayc3.avif' },
   // Left
-  { position: [-1.75, 0, 0.25], rotation: [0, Math.PI / 2.5, 0], url: '/bayc4.jpeg' },
-  { position: [-2.15, 0, 1.5], rotation: [0, Math.PI / 2.5, 0], url: '/bayc5.avif' },
-  { position: [-2, 0, 2.75], rotation: [0, Math.PI / 2.5, 0], url: '/bayc6.jpeg' },
+  { position: [-1.75, 0, 0.25], rotation: [0, Math.PI / 2.5, 0], url: '/bayc4.avif' },
+  { position: [-2.15, 0, 1.5], rotation: [0, Math.PI / 2.5, 0], url: '/bayc5.png' },
+  { position: [-2, 0, 2.75], rotation: [0, Math.PI / 2.5, 0], url: '/bayc6.avif' },
   // Right
-  { position: [1.75, 0, 0.25], rotation: [0, -Math.PI / 2.5, 0], url: '/bayc7.jpeg' },
-  { position: [2.15, 0, 1.5], rotation: [0, -Math.PI / 2.5, 0], url: '/bayc8.webp' },
-  { position: [2, 0, 2.75], rotation: [0, -Math.PI / 2.5, 0], url: '/bayc9.jpeg' }
+  { position: [1.75, 0, 0.25], rotation: [0, -Math.PI / 2.5, 0], url: '/bayc7.avif' },
+  { position: [2.15, 0, 1.5], rotation: [0, -Math.PI / 2.5, 0], url: '/bayc8.avif' },
+  { position: [2, 0, 2.75], rotation: [0, -Math.PI / 2.5, 0], url: '/bayc9.avif' }
 ]
 
 createRoot(document.getElementById('root')).render(<App images={images} />)
