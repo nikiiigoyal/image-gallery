34814ed55cf607860872de98704ef10d7f164af5
Nikita Goyal <nikiiigoyal@gmail.com>
2025-11-14 10:08:29 +0530

    Add images

--- src/index.jsx
+++ src/index.jsx
@@ -2,21 +2,21 @@ import { createRoot } from 'react-dom/client'
 import './App.css'
 import { App } from './App'
 
-const pexel = (id) => `https://images.pexels.com/photos/${id}/pexels-photo-${id}.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`
+// const pexel = (id) => `https://images.pexels.com/photos/${id}/pexels-photo-${id}.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`
 const images = [
   // Front
-  { position: [0, 0, 1.5], rotation: [0, 0, 0], url: pexel(1103970) },
+  { position: [0, 0, 1.5], rotation: [0, 0, 0], url: '/bayc1.avif' },
   // Back
-  { position: [-0.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(416430) },
-  { position: [0.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(310452) },
+  { position: [-0.8, 0, -0.6], rotation: [0, 0, 0], url: '/bayc2.png' },
+  { position: [0.8, 0, -0.6], rotation: [0, 0, 0], url: '/bayc3.jpeg' },
   // Left
-  { position: [-1.75, 0, 0.25], rotation: [0, Math.PI / 2.5, 0], url: pexel(327482) },
-  { position: [-2.15, 0, 1.5], rotation: [0, Math.PI / 2.5, 0], url: pexel(325185) },
-  { position: [-2, 0, 2.75], rotation: [0, Math.PI / 2.5, 0], url: pexel(358574) },
+  { position: [-1.75, 0, 0.25], rotation: [0, Math.PI / 2.5, 0], url: '/bayc4.jpeg' },
+  { position: [-2.15, 0, 1.5], rotation: [0, Math.PI / 2.5, 0], url: '/bayc5.avif' },
+  { position: [-2, 0, 2.75], rotation: [0, Math.PI / 2.5, 0], url: '/bayc6.jpeg' },
   // Right
-  { position: [1.75, 0, 0.25], rotation: [0, -Math.PI / 2.5, 0], url: pexel(227675) },
-  { position: [2.15, 0, 1.5], rotation: [0, -Math.PI / 2.5, 0], url: pexel(911738) },
-  { position: [2, 0, 2.75], rotation: [0, -Math.PI / 2.5, 0], url: pexel(1738986) }
+  { position: [1.75, 0, 0.25], rotation: [0, -Math.PI / 2.5, 0], url: '/bayc7.jpeg' },
+  { position: [2.15, 0, 1.5], rotation: [0, -Math.PI / 2.5, 0], url: '/bayc8.webp' },
+  { position: [2, 0, 2.75], rotation: [0, -Math.PI / 2.5, 0], url: '/bayc9.jpeg' }
 ]
 
 createRoot(document.getElementById('root')).render(<App images={images} />)
